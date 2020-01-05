e621810d1585cc9be2c4b774f91f5b827645d926
ritik sisodiya <ritik.s10120@gmail.com>
2020-01-05 08:23:17 -0800

    first commit

--- shop/views.py
+++ shop/views.py
@@ -82,7 +82,7 @@ def checkout(request):
             'INDUSTRY_TYPE_ID':'Retail',
             'WEBSITE':'WEBSTAGING',
             'CHANNEL_ID':'WEB',
-            'CALLBACK_URL':'http://127.0.0.1:8000/shop/hendlerequest/',}
+            'CALLBACK_URL':'https://damp-plateau-37623.herokuapp.com/shop/hendlerequest/',}
 		param_dict['CHECKSUMHASH']=Checksum.generate_checksum(param_dict,MERCHANT_KEY)   	
 		return render(request,'shop/paytm.html', {'param_dict':param_dict })
 
